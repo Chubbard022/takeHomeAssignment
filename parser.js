c5c42584e3301e16ff4fe6862d872bcdef9e5aaa
Chubbard022 <chubbard022@gmail.com>
2020-08-11 09:42:45 -0700

    working more on project, getting example of how data will be formatted. xml parsing working

--- parser.js
+++
@@ -1,6 +0,0 @@
-let data = import("./data")
-
-parser = new DOMParser();
-let test = parser.parseFromString(data);
-
-console.log("WORKING HERE")
--- util/parser.js
+++ util/parser.js
@@ -1,10 +1,26 @@
-let data = import("../data")
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let persistedData = {}
 
+const xhr = new XMLHttpRequest();
 
-function makeRequest(someData){
-    let newRequest = new XMLHttpRequest();
-    console.log(newRequest.readyState)
+xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4){
+        if(xhr.status === 200){
+            console.log(xhr.responseText)
+            let responseText = xhr.responseText;
+
+            let parsedData = document.getElementById("parsedData")
+
+            let newNode = document.createElement("p")
+            let text = document.createTextNode("working")
+            newNode.appendChild(text)
+            parsedData.appendChild(newNode)
+        }
+        if(xhr.status === 404){
+            console.log("file not found")
+        }
+    }
 }
 
-makeRequest()
+xhr.open("get", 'status.xml', true);
+xhr.send();
+
